f730ae4b044cc4c9d159dbe353a4aed9c9df962a
LeeCheahyun <yuipo190@gmail.com>
2021-11-30 16:59:57 +0900

    이채현

---
+++ args_paser/이채현.py
@@ -0,0 +1,29 @@
+import argparse
+
+
+args = argparse.ArgumentParser()
+
+args.add_argument('-x', '--first', required= True)
+args.add_argument('-y', '--second', required=True)
+args.add_argument('-z', '--third', required=True)
+
+argvar = vars(args.parse_args())
+
+
+def mul():
+    print(argvar['first'])
+    return argvar['first'] * argvar['second']
+    
+
+def div():
+    return argvar['second'] / argvar['third']
+
+if __name__=='__main__':
+    try:
+        print(mul())
+        print(div())
+
+    except ZeroDivisionError:
+        pass
+    except Exception as e:
+        pass
