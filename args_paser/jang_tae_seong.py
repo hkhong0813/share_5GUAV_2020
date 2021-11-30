3044fe155f2c611ddf6acfa6f75e6d23ec8a8134
bigdatachobo <bigdatachobo@gmail.com>
2021-11-30 16:47:51 +0900

    main

--- args_paser/jang_tae_seong.py
+++ args_paser/jang_tae_seong.py
@@ -10,7 +10,7 @@ parser.add_argument('-z', type=int)
 args = parser.parse_args()
 
 def multi(x,y,z):
-    return f"x * y * z = {x * y * z}"
+    return f"x * y = {x*y}\ny * z = {y*z}\nz * x = {z*x}\nx * y * z = {x * y * z}"
 
 def divide(x,y,z):
     if x == 0 or y == 0 or z == 0:
