dd9c8eacdb7efc1afaf5dc042cab5df4f381cbd0
parkheonseung <gus0tmd@naver.com>
2021-12-01 18:01:02 +0900

    sangchul 2nd commit

--- callback/sangchul_01.py
+++ callback/sangchul_01.py
@@ -1,17 +1,18 @@
 import argparse
 
-def printLength(res):
-    
+def printLength(res):    
     print('file Length : ', res)
- 
+
+def fileopen(file):
+    f=open(file, 'r')
+    lenght = len(f.read())
+    f.close()
+    printLength(lenght) 
 
 if __name__ == '__main__':
    try:
-        f=open('./testfile.txt', 'r')
-        lenght = len(f.read())
-
-        f.close()
-        printLength(lenght)
+       fileopen('./textfile.txt')
+       
    except Exception as e:
         print("오류 : ", e )
       
